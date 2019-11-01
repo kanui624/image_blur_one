4ba1e172800e464c89e662496b27ce625996b8a7
Kanui <34557865+kanui624@users.noreply.github.com>
2019-11-01 09:59:43 -0500

    Delete more_img_prac.rb

--- more_img_prac.rb
+++
@@ -1,9 +0,0 @@
-values = [["A", "B", "C"], ["D", "E", "F"], ["G", "H"]]
-
-values.each_index do |i|
-  subarray = values[i]
-  subarray.each_index do |x|
-    puts String(i) << " " << String(x) << "... " << values[i][x]
-    end
-
-end
